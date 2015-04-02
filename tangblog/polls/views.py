8e7728ff55d945ee706040010e366d632728a73f
Ryan Cotter <ryanhcotter@gmail.com>
2015-04-02 16:50:46 +0800

    More tutorial work

--- tangblog/polls/views.py
+++ tangblog/polls/views.py
@@ -1,7 +1,34 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+from polls.models import Question, Choice
 
-# Create your views here.
+def IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+def DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+def ResultsView(generic.DetailView):
+    model = Ques
+
+def vote(request, question_id):
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
